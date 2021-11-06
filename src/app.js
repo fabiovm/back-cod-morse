56d39d968f10849644c995130f33c6718056acb9
fabio.vieira.moura <fabio.vieira.moura@accenture.com>
2021-11-06 17:33:15 -0300

    Correção acesso CORS

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
+const cors = require('cors');
 
 //Rotas
 const index = require('./routes/index');
 const norseRoute = require('./routes/norseRoute');
 
+app.use(cors());
 app.use('/', index);
 app.use('/norse', norseRoute);
 module.exports = app;
